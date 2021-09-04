87d2827fd202564cd23e100f5a8810cc175ceb1d
MSPCO <sadeghpoolaee@gmail.com>
2021-09-04 13:48:05 +0430

    added User Repository interface and class.

---
+++ DigiKala.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigiKala.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        bool ExistsPermission(int permissionId, int roleId);
+        int GetUserRoleId(string username);
+    }
+}
---
+++ DigiKala.Core/Services/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using DigiKala.Core.Interfaces;
+using DigiKala.DataAccessLayer.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DigiKala.Core.Services
+{
+    public class UserRepository : IUserRepository
+    {
+        DigiKalaDbContext _context;
+        public UserRepository(DigiKalaDbContext context)
+        {
+            _context = context;
+        }
+        public bool ExistsPermission(int permissionId, int roleId)
+        {
+            return _context.RolePermissions
+                .Any(rp => rp.PermissionId == permissionId && rp.RoleId == roleId);
+        }
+
+        public int GetUserRoleId(string username)
+        {
+            return _context.Users.FirstOrDefault(u => u.Mobile == username).RoleId;
+        }
+    }
+}
