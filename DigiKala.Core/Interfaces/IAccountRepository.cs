ff792f8b117e57ee2e550aac6bcbf6b2cb6876b6
MSPCO <sadeghpoolaee@gmail.com>
2021-09-04 18:00:45 +0430

    added Account repo for registry/login

---
+++ DigiKala.Core/Interfaces/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using DigiKala.DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigiKala.Core.Interfaces
+{
+    public interface IAccountRepository
+    {
+        bool AddUser(User user);
+        bool ExistsMobileNumber(string mobileNumber);
+        int GetMaxRoleId();
+    }
+}
---
+++ DigiKala.Core/Services/AccountRepository.cs
@@ -0,0 +1,40 @@
+﻿using DigiKala.Core.Interfaces;
+using DigiKala.DataAccessLayer.Context;
+using DigiKala.DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DigiKala.Core.Services
+{
+    public class AccountRepository: IAccountRepository
+    {
+        DigiKalaDbContext _context;
+        public AccountRepository(DigiKalaDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddUser(User user)
+        {
+            try
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool ExistsMobileNumber(string mobileNumber)
+        {
+            return _context.Users.Any(u => u.Mobile == mobileNumber);
+        }
+
+        public int GetMaxRoleId() => _context.Roles.Max(r => r.Id);
+    }
+}
