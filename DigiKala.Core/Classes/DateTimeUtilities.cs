4925accbcdf2830ae3e9209a8cb2d606bfa6a8c2
MSPCO <sadeghpoolaee@gmail.com>
2021-09-04 18:00:15 +0430

    added ToShamsi method

---
+++ DigiKala.Core/Classes/DateTimeUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DigiKala.Core.Classes
+{
+    public static class DateTimeUtilities
+    {
+        public static string ToShamsi(this DateTime date)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            return persianCalendar.GetYear(date) + "/" +
+                   persianCalendar.GetMonth(date) + "/" +
+                   persianCalendar.GetDayOfMonth(date);
+        }
+    }
+}
