2af067652cf06ff2be4ce669038abd56bb5050c2
MSPCO <sadeghpoolaee@gmail.com>
2021-09-06 17:38:57 +0430

    ResetPasswordViewModel added.

---
+++ DigiKala.Core/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace DigiKala.Core.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+
+        [Display(Name = "شماره موبایل")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [Phone(ErrorMessage = "فقط عدد میتوانید وارد کنید")]
+        [MaxLength(11, ErrorMessage = "مقدار {0} نباید بیشتر از {1} کاراکتر باشد")]
+        [MinLength(11, ErrorMessage = "مقدار {0} نباید کمتر از {1} کاراکتر باشد")]
+        public string Mobile { get; set; }
+        [Display(Name = "کد فعال سازی")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [Phone(ErrorMessage = "فقط عدد میتوانید وارد کنید")]
+        [MaxLength(6, ErrorMessage = "مقدار {0} نباید بیشتر از {1} کاراکتر باشد")]
+        [MinLength(6, ErrorMessage = "مقدار {0} نباید کمتر از {1} کاراکتر باشد")]
+        public string ActivationCode { get; set; }
+        [Display(Name = "کلمه عبور جدید")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [MaxLength(50, ErrorMessage = "مقدار {0} نباید بیشتر از {1} کاراکتر باشد")]
+        [MinLength(4, ErrorMessage = "مقدار {0} نباید کمتر از {1} کاراکتر باشد")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "تایید کلمه عبور")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "کلمه های عبور همخوانی ندارد")]
+        public string ConfirmPassword { get; set; }
+    }
+}
