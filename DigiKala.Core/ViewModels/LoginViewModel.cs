292e4551b964f388708171122d622125af7a4756
MSPCO <sadeghpoolaee@gmail.com>
2021-09-06 17:37:08 +0430

    LoginViewModel added.

---
+++ DigiKala.Core/ViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace DigiKala.Core.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "شماره موبایل")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [Phone(ErrorMessage = "فقط عدد میتوانید وارد کنید")]
+        [MaxLength(11, ErrorMessage = "مقدار {0} نباید بیشتر از {1} کاراکتر باشد")]
+        [MinLength(11, ErrorMessage = "مقدار {0} نباید کمتر از {1} کاراکتر باشد")]
+        public string Mobile { get; set; }
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [MaxLength(50, ErrorMessage = "مقدار {0} نباید بیشتر از {1} کاراکتر باشد")]
+        [MinLength(4, ErrorMessage = "مقدار {0} نباید کمتر از {1} کاراکتر باشد")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "من را بخاطر بسپار")]
+        public bool RememberMe { get; set; }
+    }
+}
