c9191189d6fa51158ee25624905512a21dd94dba
MSPCO <sadeghpoolaee@gmail.com>
2021-09-06 17:42:06 +0430

    Dashboard for users added

---
+++ DigiKala.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using DigiKala.Core.Classes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiKala.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [RoleAttribute(1)]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
