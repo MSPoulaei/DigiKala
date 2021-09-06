037597c6eb49ab4a81e94b5bba3b83a568990abb
MSPCO <sadeghpoolaee@gmail.com>
2021-09-06 09:29:36 +0430

    Activate view and view model added

---
+++ DigiKala.Core/ViewModels/ActivateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace DigiKala.Core.ViewModels
+{
+    public class ActivateViewModel
+    {
+        [Display(Name = "شماره موبایل")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [Phone(ErrorMessage ="فقط عدد میتوانید وارد کنید")]
+        [MaxLength(11, ErrorMessage = "مقدار {0} نباید بیشتر از {1} کاراکتر باشد")]
+        public string Mobile { get; set; }
+        [Display(Name = "کد فعال سازی")]
+        [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [Phone(ErrorMessage ="فقط عدد میتوانید وارد کنید")]
+        [MaxLength(6, ErrorMessage = "مقدار {0} نباید بیشتر از {1} کاراکتر باشد")]
+        public string ActivationCode { get; set; }
+    }
+}
