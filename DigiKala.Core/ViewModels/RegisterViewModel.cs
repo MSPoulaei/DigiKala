69f84f35fd02edb0113deb99b58531846cf9eb82
MSPCO <sadeghpoolaee@gmail.com>
2021-09-06 17:37:40 +0430

    password length changed to 50.

--- DigiKala.Core/ViewModels/RegisterViewModel.cs
+++ DigiKala.Core/ViewModels/RegisterViewModel.cs
@@ -15,7 +15,7 @@ namespace DigiKala.Core.ViewModels
         public string Mobile { get; set; }
         [Display(Name = "کلمه عبور")]
         [Required(ErrorMessage = "نباید بدون مقدار باشد")]
-        [MaxLength(100, ErrorMessage = "مقدار {0} نباید بیشتر از {1} کاراکتر باشد")]
+        [MaxLength(50, ErrorMessage = "مقدار {0} نباید بیشتر از {1} کاراکتر باشد")]
         [MinLength(4, ErrorMessage = "مقدار {0} نباید کمتر از {1} کاراکتر باشد")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
