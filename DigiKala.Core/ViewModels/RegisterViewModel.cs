26cb4e3a1f3070f8059f7a88192679f31ce36a6a
MSPCO <sadeghpoolaee@gmail.com>
2021-09-06 09:29:10 +0430

    register view and view model added.

--- DigiKala.Core/ViewModels/RegisterViewModel.cs
+++ DigiKala.Core/ViewModels/RegisterViewModel.cs
@@ -9,6 +9,7 @@ namespace DigiKala.Core.ViewModels
     {
         [Display(Name = "شماره موبایل")]
         [Required(ErrorMessage = "نباید بدون مقدار باشد")]
+        [Phone(ErrorMessage ="فقط عدد میتوانید وارد کنید")]
         [MaxLength(11, ErrorMessage = "مقدار {0} نباید بیشتر از {1} کاراکتر باشد")]
         [MinLength(11, ErrorMessage = "مقدار {0} نباید کمتر از {1} کاراکتر باشد")]
         public string Mobile { get; set; }
